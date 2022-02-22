202eee7afa8a7670b10b3c4fc9ee28d465c683f4
John Ouellet <john@thinktandem.io>
2022-02-22 13:55:17 -0500

    Update to new docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,26 +29,6 @@ module.exports = {
         link: '/index.md',
       },
       '/config.md',
-      '/caveats.md',
-      '/extensions.md',
-      {
-        text: 'Guides',
-        collapsible: true,
-        children: [
-          {
-            text: 'Accessing Elasticsearch Logs',
-            link: '/accessing-logs.md',
-          },
-          {
-            text: 'Installing extensions',
-            link: '/installing-extensions.md',
-          },
-          {
-            text: 'Installing node',
-            link: '/installing-node.md',
-          },
-        ],
-      },
       '/support.md',
       {text: 'Examples', link: 'https://github.com/lando/elasticsearch/tree/main/examples'},
       {text: 'Release Notes', link: 'https://github.com/lando/elasticsearch/releases'},
